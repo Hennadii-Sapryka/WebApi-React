910975c7f8a60b183f9121cb095d61d66a74d15d
Hennadii Sapryka <hennadii.sapryka@gmail.com>
2023-05-26 16:30:33 +0300

    added some features

---
+++ src/Map/MyMap.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+
+import useStyles from './styles.js';
+
+export const MyMap = ({  places, setCoordinates, coordinates, setBounds, setChildClicked }) => {
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        center={coordinates}
+        mapContainerStyle={classes.mapContainer}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true }}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+      >
+         {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={place.latitude}
+            lng={place.longitude}
+            key={i}
+          >
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography  variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
+
+
+      </GoogleMapReact>
+    </div>
+  );
+};
+
