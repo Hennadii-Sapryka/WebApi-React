6925c571b88f51e931acc370119b3942be89e817
Hennadii Sapryka <hennadii.sapryka@gmail.com>
2023-06-23 15:14:16 +0300

    added filter

--- src/App.js
+++ src/App.js
@@ -9,52 +9,67 @@ import { MyHeader } from './Header/MyHeader.jsx';
 import { MyMap } from './Map/MyMap.jsx';
 import { List } from './List/List.jsx';
 import { CssBaseLine, Grid } from '@material-ui/core/';
-import {getPlacesData}  from './api/index.js';
-import {dataOfRestaurants} from './List/dataOfRest.js';
+import { getPlacesData } from './api/index.js';
+import { dataOfRestaurants } from './List/dataOfRest.js';
 
 const App = () => {
 
   const [users, setUsers] = useState([]);
-  const [places, setPlaces] = useState([]); 
+  const [places, setPlaces] = useState([]);
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
   const [type, setType] = useState('restaurant');
   const [rating, setRating] = useState('');
-  const [childClicked, setChildClicked]=useState(null);
+  const [childClicked, setChildClicked] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [filteredPlaces, setFilteredPlaces] = useState([]);
   const [autocomplete, setAutocomplete] = useState(null);
-  
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyC_xxE0PCh6jr1dn4M1RFcRA5hLs6urffg"
   });
 
-/*   useEffect(() => {
-    fetch(ApiConfig.API_URL2)
-      .then(response => response.json())
-      .then(responseJson => {
-        setElec(responseJson)
-      })
-  }, []) */
+  /*   useEffect(() => {
+      fetch(ApiConfig.API_URL2)
+        .then(response => response.json())
+        .then(responseJson => {
+          setElec(responseJson)
+        })
+    }, []) */
 
+  let testPlace = dataOfRestaurants.map((place) => String(place.name));
+
+  const [value, setValue] = useState(null);
+  console.log(value);
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+    setCoordinates({ lat, lng });
+  };
+
+  const onPlaceSearch = () => {
+    let filtersearch = dataOfRestaurants.filter((place) => place.name === value);
+    setFilteredPlaces(filtersearch);
+  };
 
   useEffect(() => {
     setIsLoading(true);
 
     getPlacesData(type, bounds.sw, bounds.ne) //recive restourants
-    .then((data) => { 
-      setPlaces(dataOfRestaurants.filter((place) => place.name && place.num_reviews > 0));
-      setFilteredPlaces([]);
-      
-      setIsLoading(false);
-    })
+      .then((data) => {
+        setPlaces(dataOfRestaurants.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      })
   }, [type, coordinates, bounds]);
 
   useEffect(() => {
     const filtered = places.filter((place) => Number(place.rating) > rating);
-
     setFilteredPlaces(filtered);
-  }, [rating]);
+  }, [rating, value]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
@@ -65,31 +80,40 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <MyHeader></MyHeader>
+        <MyHeader
+          id={testPlace.id}
+          options={testPlace}
+          value={value}
+          onChange={(_, newValue) => {
+            setValue(newValue);
+            onPlaceSearch();
+          }}
+        >
+        </MyHeader>
         <Grid container spacing={3} style={{ width: '100%' }}>
           <Grid item xs={12} md={4}>
-            <List 
-            places={filteredPlaces.length ? filteredPlaces : places}
-            type={type}
-            setType={setType}
-            rating={rating}
-            setRating={setRating}
-            childClicked={childClicked}
-            isLoading={isLoading}
-             />
+            <List
+              places={filteredPlaces.length ? filteredPlaces : places}
+              type={type}
+              setType={setType}
+              rating={rating}
+              setRating={setRating}
+              childClicked={childClicked}
+              isLoading={isLoading}
+            />
           </Grid>
           <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 
-            {isLoaded ? 
-            <MyMap 
-            setBounds={setBounds}
-            setCoordinates={setCoordinates}
-            coordinates={coordinates}
-            bounds={bounds}
-            places={filteredPlaces.length ? filteredPlaces : places}
-            setChildClicked={setChildClicked}
-            
-            /> : <h3>Loading...</h3>}
+            {isLoaded ?
+              <MyMap
+                setBounds={setBounds}
+                setCoordinates={setCoordinates}
+                coordinates={coordinates}
+                bounds={bounds}
+                places={filteredPlaces.length ? filteredPlaces : places}
+                setChildClicked={setChildClicked}
+
+              /> : <h3>Loading...</h3>}
           </Grid>
         </Grid>
       </header>
--- src/Header/MyHeader.jsx
+++ src/Header/MyHeader.jsx
@@ -1,34 +1,46 @@
-import React from 'react';
-import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import React, { useState } from 'react';
+
+import { Autocomplete } from '@mui/material';
+import { AppBar, Toolbar, Typography, InputBase, Box, TextField } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles.js';
 
-export const MyHeader = ({ onPlaceChanged, onLoad }) => {
+export const MyHeader = ({ options, onClick, onChange, value }) => {
   const classes = useStyles();
+
 
   return (
     <AppBar position="static">
-    <Toolbar className={classes.toolbar}>
-      <Typography variant="h5" className={classes.title}>
-        Travel Advisor
-      </Typography>
-      <Box display="flex">
-        <Typography variant="h6" className={classes.title}>
-          Explore new places
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
         </Typography>
-        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete
+          id="combo-box"
+            renderInput={(props) => <TextField {...props} id={props.InputProps.id} />}
+            options={options}
+            value={value}
+            onChange={onChange}
+            style={{width:250}}
+          >
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
             </div>
-            <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-          </div>
-        </Autocomplete>
-      </Box>
-    </Toolbar>
-  </AppBar>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
